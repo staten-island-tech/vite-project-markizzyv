669fd1cbf009ab52c455de6474da15a2c2d49dd1
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-11-20 09:24:02 -0500

    s
    s

---
+++ vite-project/menu.js
@@ -0,0 +1,32 @@
+const filipaneseMenu = [
+    {
+        itemName: "Pork Sinigang",
+        itemType: "Main Dish",
+        itemDesc:  "pork with vegetables in tamarind based soup",
+        itemPrice: "$45",
+        itemImg: "",
+
+    },
+   {
+        itemName: "Beef Sinigang",
+        itemType: "Main Dish",
+        itemDesc:  "beef with vegetables in tamarind based soup",
+        itemPrice: "$45",
+        itemImg: "",
+   },
+   {
+        itemName: "Lumpia Pork/Chicken",
+        itemType: "Appeitzer",
+        itemDesc: "crispy eggrolls with pork or chicken and vegetables",
+        itemPrice: "$9.50",
+        itemImg: "",
+   },
+   {
+        itemName: "Bulaklak",
+        itemType: "Appeitzer",
+        itemDesc: "crispy pork slices",
+        itemPrice: "$8.50",
+        itemImg: "",
+   }
+]
+
