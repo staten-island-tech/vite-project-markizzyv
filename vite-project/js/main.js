1725b23b1665685d7e6d506cfadcf42bd20ff4e9
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-12-04 09:22:59 -0500

    n
    n

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,24 +1,46 @@
 import '../css/style.css'
-import javascriptLogo from './javascript.svg'
+/* import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { setupCounter } from './counter.js' */
 
-document.querySelector('#app').innerHTML = `
+
+
+
+
+//imports
+import { DOMSelectors } from '../dom.js'
+import { filipaneseMenu } from './menu.js'
+//create card
+function Insert (arr){
+  arr.forEach((fdish)=> fdish.document.querySelector(".flex-container").insertAdjacentHTML("afterbegin",
+ 
+
+
+  //h1 class diff
+  `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
+  <h1 class ="fdishTitle">${fdish.itemName}</h1>  
+  <h2 class ="fdishType">${fdish.itemType}</h2>
+  <p class ="fdishDesc">${fdish.itemDesc}</p>
+  <h3 class ="fdishPrice">${fdish.itemPrice}</h3>
+  <img src = "${fdish.itemImg}" class ="fdishImg">
   </div>
-`
+  `
+  ));
+}
+Insert (filipaneseMenu)
 
-setupCounter(document.querySelector('#counter'))
+
+
+
+//filter appe
+const appe = function appeFilter () {
+  filipaneseMenu.filter((filipaneseMenu) => filipaneseMenu.itemType.includes("Appetizer"));
+
+
+  DOMSelectors.appebtn.addEventListener("click", (event) =>
+  event.preventDefault(),
+
+
+  Insert(appe)
+)};
