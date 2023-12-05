eee527e51062b68ca70da0e28f607b75943f1105
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-12-04 19:03:28 -0500

    v
    v

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,7 +1,7 @@
-import './style.css'
+ import './style.css' 
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+ 
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -11,14 +11,11 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
+
+
+
+ 
+
