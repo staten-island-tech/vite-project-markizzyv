fde5c960ae2d0b9d38e561ff1ebfaf4718ada6bb
markizzyv <144455870+markizzyv@users.noreply.github.com>
2023-12-05 08:49:52 -0500

    Revert "Revert "d""
    This reverts commit 66fad78b64b466c9f3b0ee5be4599218d3730867.

--- vite-project/dom.js
+++ vite-project/dom.js
@@ -4,6 +4,7 @@ const DOMSelectors = {
     appebtn: document.querySelector(".appetizers"),
     mainDishBTN: document.querySelector(".mainDish"),
     desse: document.querySelector(".dessert"),
-    drinkz: document.querySelector(".drinks")
+    drinkz: document.querySelector(".drinks"),
+    themebtn: document.querySelector(".theme")
 }
 export {DOMSelectors}
